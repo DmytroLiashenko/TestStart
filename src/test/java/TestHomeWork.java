83584c7fb265eede1f4f6d09d7f2d5d044400023
Dmytro <dzima1233@gmail.com>
2023-05-30 19:14:19 +0300

    HomeWork11

--- src/test/java/TestHomeWork.java
+++ src/test/java/TestHomeWork.java
@@ -8,6 +8,9 @@ import java.time.Duration;
 
 public class TestHomeWork extends BaseClass {
     public static void main(String[] args) throws InterruptedException {
+        String name = "Dmytro";
+        String email = "test@gmail.com";
+
         driver.get("https://ithillel.ua/");
         System.out.println(driver.getTitle());
         System.out.println(driver.getCurrentUrl());
@@ -16,16 +19,25 @@ public class TestHomeWork extends BaseClass {
         WebElement consultButton = driver.findElement(By.id("btn-consultation-hero"));
         System.out.println(consultButton.getText());
         consultButton.click();
-        driver.findElement(By.id("input-name-consultation")).sendKeys("Dmytro");
-        driver.findElement(By.id("input-email-consultation")).sendKeys("test@gmail.com");
+        driver.findElement(By.id("input-name-consultation")).sendKeys(name);
+        driver.findElement(By.id("input-email-consultation")).sendKeys(email);
         driver.findElement(By.id("input-tel-consultation")).sendKeys("509544567");
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@id=\"field-messenger-consultation\"]/div/div/div/div[2]/button[1]")).click();
-        driver.findElement(By.xpath("//*[@id=\"field-messenger-consultation\"]/div/div/div/div[1]/div/div[2]/input")).sendKeys("https://t.me/hyevuy_dnepr");
-        driver.findElement(By.id("listbox-btn-input-course-consultation")).click();
-        driver.findElement(By.xpath("//*[@id=\"container-input-course-consultation\"]/div/ul/li[19]")).click();
 
-        driver.findElement(By.xpath("//*[@id=\"form-consultation\"]/div[1]/footer/div[1]/label/span")).click();
+        Thread.sleep(2000);
+        //Working with Telegram
+        driver.findElement(By.xpath("//button[@data-name=\"tg\"]")).click();
+        driver.findElement(By.xpath("//input[@name=\"tg\"]")).sendKeys("https://t.me/hyevuy_dnepr");
+
+// Working on the dropdown
+        driver.findElement(By.id("listbox-btn-input-course-consultation")).click();
+        driver.findElement(By.xpath("//*[@data-entry-id=\"129290\"]")).click();
+
+//Working on the checkbox
+//        driver.findElement(By.linkText("обробку персональних даних")).click();
+//        driver.switchTo().window(String.valueOf(0));
+
+
+        driver.findElement(By.xpath("//*[@class=\"checkbox_checkmark\"]")).click();
 
 
 
